5a3547bdd2d793b86bc03d29cab16f983d3ea478
Gopal Pokhrel <pokhrelgopal27@gmail.com>
2024-02-17 18:01:26 +0545

    flask-project-setub-basic

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,31 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import send_from_directory
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+from flask import Flask
 import os
 
 load_dotenv()
 
+
+# ! Set up media folder
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+MEDIA_FOLDER = os.path.join(os.getcwd(), "media")
+MEDIA = os.path.join("media", "uploads")
+
+"""
+If you need more folder to store media files, you can create more variables like MEDIA_FOLDER and MEDIA, with the path to the folder you want to store the media files.
+"""
+
+
 app = Flask(__name__)
 from app import routes
+
+
+# ! Serve media files
+@app.route("/media/<path:filename>")
+def media_files(filename):
+    return send_from_directory(MEDIA_FOLDER, filename)
 
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
